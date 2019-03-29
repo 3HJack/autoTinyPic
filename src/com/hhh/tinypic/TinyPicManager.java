f2f35f281fcb7c8f49b68e9a81b97a5d5986a2e8
huanghaihua <huanghaihua@kuaishou.com>
2019-03-29 14:25:48 +0800

    If icon is greater than 10KB, Warnging user

--- src/com/hhh/tinypic/TinyPicManager.java
+++ src/com/hhh/tinypic/TinyPicManager.java
@@ -1,5 +1,6 @@
 package com.hhh.tinypic;
 
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.vfs.VirtualFile;
@@ -7,17 +8,26 @@ import com.intellij.openapi.vfs.VirtualFileEvent;
 import com.intellij.openapi.vfs.VirtualFileListener;
 import com.intellij.openapi.vfs.VirtualFileManager;
 import com.tinify.Tinify;
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang.StringUtils;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.File;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.prefs.Preferences;
 
 public class TinyPicManager {
+    public static final int MAX_COMPRESS_COUNT_PER_MONTH = 500;
+    public static final long MAX_SIZE_FOR_WARNING = 1024 * 10L;
+
     public static final String KEY_DIALOG_TITLE = "autoTinyPic";
     public static final String KEY_API = "tiny_pic_api";
 
     private static TinyPicManager ourInstance = new TinyPicManager();
+    private final ExecutorService mExecutorService = Executors.newCachedThreadPool();
 
+    private Project mProject;
     private VirtualFileListener mFileListener;
 
     private TinyPicManager() {
@@ -28,6 +38,7 @@ public class TinyPicManager {
     }
 
     public void initTinyPic(Project project) {
+        mProject = project;
         String apiKey = Preferences.userRoot().get(KEY_API, "");
         if (StringUtils.isEmpty(apiKey)) {
             unInitTinyPic();
@@ -41,15 +52,14 @@ public class TinyPicManager {
             Messages.showMessageDialog(project, "invalid key!!!", KEY_DIALOG_TITLE, Messages.getErrorIcon());
             return;
         }
+        if (Tinify.compressionCount() > MAX_COMPRESS_COUNT_PER_MONTH) {
+            Messages.showMessageDialog(project, "your compression limit is reached!!!", KEY_DIALOG_TITLE, Messages.getWarningIcon());
+            return;
+        }
         mFileListener = new VirtualFileListener() {
             @Override
-            public void contentsChanged(@NotNull VirtualFileEvent event) {
-                compressPicFile(event.getFile());
-            }
-
-            @Override
             public void fileCreated(@NotNull VirtualFileEvent event) {
-                compressPicFile(event.getFile());
+                mExecutorService.submit(() -> compressPicFile(event.getFile()));
             }
         };
         VirtualFileManager.getInstance().addVirtualFileListener(mFileListener);
@@ -62,7 +72,7 @@ public class TinyPicManager {
         }
     }
 
-    private void compressPicFile(VirtualFile file) {
+    public void compressPicFile(VirtualFile file) {
         String fileName = file.getName();
         if (file.isDirectory() || !(fileName.endsWith(".png") || fileName.endsWith(".jpg"))) {
             return;
@@ -70,6 +80,18 @@ public class TinyPicManager {
         try {
             String filePath = file.getPath();
             Tinify.fromFile(filePath).toFile(filePath);
+            if (FileUtils.sizeOf(new File(filePath)) > MAX_SIZE_FOR_WARNING) {
+                ApplicationManager.getApplication().invokeLater(() -> {
+                    int result = Messages.showYesNoDialog(mProject, filePath + " is greater than 10KB !!! Are you sure to keep it ?", KEY_DIALOG_TITLE, Messages.getQuestionIcon());
+                    if (result == Messages.NO) {
+                        try {
+                            file.delete(null);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
